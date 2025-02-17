152738c5c1cf5410861ecbd99576fae8166ac1e2
elsenorbw <nope@nowhere.com>
2025-02-17 22:09:51 +0000

    Harpoon2 working

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local harpoon = require("harpoon")
+--
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+--
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end, { desc = "[A]dd to harpoon" })
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Toggle Harpoon List" })
+--
+vim.keymap.set("n", "<C-a>", function()
+  harpoon:list():select(1)
+end, { desc = "Select 1st Harpoon File" })
+vim.keymap.set("n", "<C-b>", function()
+  harpoon:list():select(2)
+end, { desc = "Select 2nd Harpoon File" })
+vim.keymap.set("n", "<C-c>", function()
+  harpoon:list():select(3)
+end, { desc = "Select 3rd Harpoon File" })
+vim.keymap.set("n", "<C-d>", function()
+  harpoon:list():select(4)
+end, { desc = "Select 4th Harpoon File" })
---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    keys = function()
+      return {}
+    end,
+  },
+}
